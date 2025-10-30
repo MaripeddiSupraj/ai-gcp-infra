e15cc985094b0681a2b441152243c070cc9e8bfe
MaripeddiSupraj <supraj.maripeddi@gmail.com>
2025-10-30 18:25:57 +0530

    Add mixed on-demand and spot node pools, fix terraform-apply trigger

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -51,8 +51,62 @@ resource "google_container_cluster" "primary" {
   deletion_protection = false
 }
 
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "${var.cluster_name}-node-pool"
+# On-demand node pool for critical workloads
+resource "google_container_node_pool" "on_demand" {
+  name       = "${var.cluster_name}-on-demand"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+  project    = var.project_id
+
+  node_config {
+    machine_type = var.machine_type
+    disk_size_gb = var.disk_size_gb
+    disk_type    = "pd-standard"
+    spot         = false
+
+    labels = {
+      workload-type = "on-demand"
+    }
+
+    taint {
+      key    = "workload-type"
+      value  = "on-demand"
+      effect = "NO_SCHEDULE"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+
+    workload_metadata_config {
+      mode = "GKE_METADATA"
+    }
+
+    shielded_instance_config {
+      enable_secure_boot          = true
+      enable_integrity_monitoring = true
+    }
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  upgrade_settings {
+    max_surge       = 1
+    max_unavailable = 0
+  }
+}
+
+# Spot node pool for cost-effective workloads
+resource "google_container_node_pool" "spot" {
+  name       = "${var.cluster_name}-spot"
   location   = var.region
   cluster    = google_container_cluster.primary.name
   node_count = var.node_count
@@ -62,6 +116,17 @@ resource "google_container_node_pool" "primary_nodes" {
     machine_type = var.machine_type
     disk_size_gb = var.disk_size_gb
     disk_type    = "pd-standard"
+    spot         = true
+
+    labels = {
+      workload-type = "spot"
+    }
+
+    taint {
+      key    = "workload-type"
+      value  = "spot"
+      effect = "NO_SCHEDULE"
+    }
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
