7890f9881f8bda635bff414a849d5d21a1a0bb88
MaripeddiSupraj <supraj.maripeddi@gmail.com>
2025-10-31 08:58:27 +0530

    test: add outputs to trigger terraform workflow
    Added project_id, region, and repository_id outputs for testing

--- outputs.tf
+++ outputs.tf
@@ -14,3 +14,18 @@ output "cluster_endpoint" {
 output "repository_url" {
   value = module.gar.repository_url
 }
+
+output "project_id" {
+  value       = var.project_id
+  description = "GCP Project ID"
+}
+
+output "region" {
+  value       = var.region
+  description = "GCP Region"
+}
+
+output "repository_id" {
+  value       = var.repository_id
+  description = "Artifact Registry Repository ID"
+}
