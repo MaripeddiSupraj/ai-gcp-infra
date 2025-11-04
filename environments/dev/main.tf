7e99d833e1c8da7257a4dc1a0244bfa12a445307
MaripeddiSupraj <supraj.maripeddi@gmail.com>
2025-11-04 23:41:41 +0530

    fix: use project number for GKE service account

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -62,13 +62,18 @@ module "gar" {
   description   = var.gar_description
 }
 
+# Get project number for service account
+data "google_project" "project" {
+  project_id = var.project_id
+}
+
 # Grant GKE nodes access to pull images from Artifact Registry
 resource "google_artifact_registry_repository_iam_member" "gke_reader" {
   project    = var.project_id
   location   = var.region
   repository = module.gar.repository_id
   role       = "roles/artifactregistry.reader"
-  member     = "serviceAccount:${var.project_id}-compute@developer.gserviceaccount.com"
+  member     = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
 }
 
 # Temporarily disabled - enable after GKE cluster is created
