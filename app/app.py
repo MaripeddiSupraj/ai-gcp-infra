97075573fc572d5f63a1cb9cb38160e07caab622
MaripeddiSupraj <130624939+MaripeddiSupraj@users.noreply.github.com>
2025-11-09 04:51:56 +0530

    Feature/k8s deploy workflow (#21)
    * feat: add K8s deployment workflow for GKE
    
    * fix: add gcloud setup with kubectl auth plugin

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ def health():
 
 @app.route('/version')
 def version():
-    return {'version': '1.0.2', 'app': 'gke-demo', 'environment': 'production'}
+    return {'version': '1.0.3', 'app': 'gke-demo', 'environment': 'production'}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
